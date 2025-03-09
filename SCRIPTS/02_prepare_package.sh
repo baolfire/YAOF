bddc10b7340f6992f3ca7df8611d6fa0528278f2
baolfire <baolfire@users.noreply.github.com>
2025-03-09 22:39:36 +0800

    Update 02_prepare_package.sh

--- SCRIPTS/02_prepare_package.sh
+++ SCRIPTS/02_prepare_package.sh
@@ -144,6 +144,7 @@ sed -i 's,@CMDLINE@ noinitrd,noinitrd mitigations=off,g' target/linux/x86/image/
 
 ### ADD PKG 部分 ###
 cp -rf ../OpenWrt-Add ./package/new
+cp -rf ../OpenWrt-Add/openwrt_pkgs/luci-app-adguardhome ./package/new/luci-app-adguardhome
 rm -rf feeds/packages/net/{xray-core,v2ray-core,v2ray-geodata,sing-box,frp,microsocks,shadowsocks-libev,zerotier,daed}
 rm -rf feeds/luci/applications/{luci-app-frps,luci-app-frpc,luci-app-zerotier,luci-app-filemanager}
 rm -rf feeds/packages/utils/coremark
